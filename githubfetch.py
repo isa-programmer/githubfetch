26c147196c0ed9e5de23b1f4d83fe338254385e9
Fuan200 <juandiazfdez1992@gmail.com>
2025-06-10 11:51:41 -0600

    implementing functional programming

--- githubfetch.py
+++ githubfetch.py
@@ -20,50 +20,65 @@ class Color:
         
     def color(self,color_name,text):
         return f"{color_name}{text}{color.reset}"
-        
-color = Color()
-indent = " " * 22
-username = sys.argv[1]
-github_url = f"{username}@github.com"
-url = f"https://api.github.com/users/{username}"
-response = requests.get(url)
+    
+def get_user_data(username):
+    user_url = f"https://api.github.com/users/{username}"
+    response = requests.get(user_url)
+    return response.json()
 
-if not response.ok:
-    print(f"{color.red}Error:{response.status_code}")
-    sys.exit(1)
+def get_starred_count(username):
+    user_url = f"https://api.github.com/users/{username}/starred"
+    response = requests.get(user_url)
+    return response.json()
 
-data = response.json()
-
-starred_url = f"https://api.github.com/users/{username}/starred"
-starred_repos = requests.get(starred_url) 
-data_starred = starred_repos.json()
-starred_count = len(data_starred)
-
-try:
-    subprocess.run([
+def display_avatar(image_url):
+    try:
+        subprocess.run([
                 "kitten", "icat", "--align",
                 "left", "--scale-up", "--place",
-                "20x20@0x0", data.get('avatar_url')])
+                "20x20@0x0", image_url])
     
-except FileNotFoundError:
-    print(color.red,"Kitty Terminal not installed!", color.reset)
-    sys.exit(1)
-    
-elements = [
-    {"text":color.color(color.light_blue,"Username:"),"value":data.get('login')},
-    {"text":color.color(color.yellow,"Repos:"),"value":data.get('public_repos')},
-    {"text":color.color(color.green,"Bio:"),"value":data.get('bio','N/A') or 'N/A'},
-    {"text":color.color(color.red,"From:"),"value":data.get('location','Not Provided')},
-    {"text":color.color(color.light_red,"Followers:"),"value":data.get('followers')},
-    {"text":color.color(color.light_blue,"Following:"),"value":data.get('following')},
-    {"text":color.color(color.yellow,"Starred repos:"),"value":starred_count},
-]
+    except FileNotFoundError:
+        print(color.red,"Kitty Terminal not installed!", color.reset)
+        sys.exit(1)
 
+def display_user_info(data, starred_count, username):
+    github_url = f"{username}@github.com"
+    indent = " " * 22
 
-print(f"{indent} {github_url}")
-print(f"{indent} {'-'*len(github_url)}")
+    elements = [
+        {"text":color.color(color.light_blue,"Username:"),"value":data.get('login')},
+        {"text":color.color(color.yellow,"Repos:"),"value":data.get('public_repos')},
+        {"text":color.color(color.green,"Bio:"),"value":data.get('bio','N/A') or 'N/A'},
+        {"text":color.color(color.red,"From:"),"value":data.get('location','Not Provided')},
+        {"text":color.color(color.light_red,"Followers:"),"value":data.get('followers')},
+        {"text":color.color(color.light_blue,"Following:"),"value":data.get('following')},
+        {"text":color.color(color.yellow,"Starred repos:"),"value":starred_count},
+    ]
 
-for element in elements:
-    print(f"{indent} {element['text']} {element['value']}")
+    print(f"{indent} {github_url}")
+    print(f"{indent} {'-'*len(github_url)}")
 
-print("\n\n\n")
+    for element in elements:
+        print(f"{indent} {element['text']} {element['value']}")
+
+    print("\n")
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: githubfetch <your-github-username>")
+        sys.exit(1)
+
+    color = Color()
+    username = sys.argv[1]
+
+    try:
+        user_data = get_user_data(username)
+        starred_count = get_starred_count(username)
+        data_starred = len(starred_count)
+        display_avatar(user_data.get('avatar_url'))
+        display_user_info(user_data, data_starred, username)
+
+    except Exception as e:
+        print(color.color(color.red, str(e)))
+        sys.exit(1)
